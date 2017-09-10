4c1f0c06f2a09d8486fc60855f7195680407e856
AnasAttahir <31764833+AnasAttahir@users.noreply.github.com>
2017-09-10 21:47:42 +0200

    Initial commit

---
+++ exercise 2_1.py
@@ -0,0 +1,3 @@
+letters = ('A', 'C', 'B', 'B', 'C', 'A', 'C', 'C', 'B')
+letters_list = list(letters)
+print ([letters_list.count('A'), letters_list.count('B'), letters_list.count('C')])
---
+++ exercise 2_2.py
@@ -0,0 +1,13 @@
+ICOR = 8
+PROG = 6
+CSN = 7
+
+gemiddelde = (ICOR + PROG + CSN)/ 3
+beloning = (ICOR*30)+(PROG*30)+(CSN*30)
+print(gemiddelde)
+print(beloning)
+overzicht = ('Mijn cijfers' + ' (gemiddeld een 7 )leveren een beloning van' + '630 op!')
+
+gemiddelde = str(gemiddelde)
+beloning = str(beloning)
+print(overzicht)
